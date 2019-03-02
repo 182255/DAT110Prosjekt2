b6cf381749af0c3e069d2f98bb88bf756fd3e590
Enah <182255@stud.hvl.no>
2019-03-02 02:43:09 +0100

    removed todo

--- project2/src/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ project2/src/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -3,33 +3,28 @@ package no.hvl.dat110.iotsystem;
 import no.hvl.dat110.client.Client;
 
 public class TemperatureDevice {
-	
+
 	private static final int COUNT = 10;
-	
+
 	public static void main(String[] args) {
-		
+
 		TemperatureSensor sn = new TemperatureSensor();
-		Client client = new Client("TemperatureDevice", Common.BROKERHOST, Common.BROKERPORT); 
+		Client client = new Client("TemperatureDevice", Common.BROKERHOST, Common.BROKERPORT);
 		client.connect();
-		// TODO - start
-		
-		for(int i = 0; i < COUNT; i++) {
+		for (int i = 0; i < COUNT; i++) {
 			int temp = sn.read();
 			client.publish(Common.TEMPTOPIC, Integer.toString(temp));
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 		client.disconnect();
-		// TODO - end
-		
+
 		System.out.println("Temperature device stopping ... ");
-		
-//		throw new RuntimeException("not yet implemented");
-		
+
 	}
+
 }
